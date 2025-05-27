e6aa50e703e42383173993b7db0bef9cf3aadfb0
Josias <ndjosiasaurel@gmail.com>
2025-05-27 13:13:34 +0000

    add script for running programs (incomplete)

---
+++ run-app.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# first argument is a link to the github repository
+# repo_link=$1
+
+# project name - it will read from the submissions folder by default
+# project name is expected to have no space
+project_name=$1
+
+# IFS='/' read -ra parts <<< "$repo_link"
+
+if [ ! -d "terminalcraft" ]; then
+    echo "TerminalCraft Repo does not exist, cloning..."
+    git clone https://github.com/hackclub/terminalcraft/
+fi
+
+cd terminalcraft
+cd "submissions/$project_name"
+ls
+
+if [ -f "pyproject.toml" ]; then
+    # this should be the main command
+    uv run main.py
+fi
+
+# should add other commands here
