70d41744b9bf2c4bb03bdc909537fe537fd39f5c
Josias <ndjosiasaurel@gmail.com>
2025-06-04 08:55:06 +0000

    remove sudo as useless and install python uv runner

--- add-ttyd.sh
+++ add-ttyd.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y build-essential cmake git libjson-c-dev libwebsockets-dev
+apt-get update
+apt-get install -y build-essential cmake git libjson-c-dev libwebsockets-dev
 git clone https://github.com/tsl0922/ttyd.git
 cd ttyd && mkdir build && cd build
 cmake ..
 make && make install
+
+# also install python uv for some packages 
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+pip install uv
+
+# ttyd
