ab179a3c035211f0a5fa69aac5c24b321d6f3fbb
Josias <ndjosiasaurel@gmail.com>
2025-06-09 17:56:02 +0100

    generate caddy server config when creating new service

--- expose.py
+++ expose.py
@@ -38,9 +38,39 @@ def _run_app(project_name: str):
     print("Will be running on port", project_port)
     run_cmd = f"docker run -p {project_port}:8989 jos/palm {project_name}"
     ps = subprocess.Popen(run_cmd.split(" "))
+    print("running command")
+    print(run_cmd)
     # print(ps)
 
     # after the project has been started, add it to the in-memory store
     projects_store[project_name] = [project_port, ps]
 
+
+    # regenerate caddy config such that it includes the newly added proejct
+    regenerate_caddy_config()
     return { "success": True, "port": project_port } 
+
+# this function will regenerate the caddy config based on the newly added proejct paths and their ports
+def regenerate_caddy_config() -> None:
+    caddy_config = """
+        localhost {
+            handle {
+                respond "TerminalCraft Project Demo"
+            }
+        }
+    """
+
+    for project_name, project_info in projects_store.items():
+        project_caddy_config = f"""
+        {project_name}.localhost {{
+            reverse_proxy localhost:{project_info[0]}
+        }}
+        """
+
+        caddy_config += project_caddy_config
+
+    with open('Caddyfile', "w") as config_f:
+        config_f.write(caddy_config)
+
+    # logging the newly generated caddy config soley for debugging purposes
+    print(caddy_config)
