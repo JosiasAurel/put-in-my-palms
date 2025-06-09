9f5b8b36d175ec8ee31c2ddbae0dd93c9f4f7fa5
Josias <ndjosiasaurel@gmail.com>
2025-06-09 18:47:24 +0100

    generate caddy config on startup

--- expose.py
+++ expose.py
@@ -25,6 +25,37 @@ def assign_port() -> int:
 # ProjectName:[ProjectPort, ChildProcess]
 projects_store = {}
 
+# this function will regenerate the caddy config based on the newly added proejct paths and their ports
+def regenerate_caddy_config() -> None:
+    caddy_config = f"""
+        {ROOT_DOMAIN} {{
+            handle {{
+                reverse_proxy localhost:{APP_PORT}
+                respond "TerminalCraft Project Demo"
+            }}
+        }}
+    """
+
+    for project_name, project_info in projects_store.items():
+        project_caddy_config = f"""
+        {project_name}.{ROOT_DOMAIN} {{
+            reverse_proxy localhost:{project_info[0]}
+        }}
+        """
+
+        caddy_config += project_caddy_config
+
+    with open('Caddyfile', "w") as config_f:
+        config_f.write(caddy_config)
+
+    # logging the newly generated caddy config soley for debugging purposes
+    print(caddy_config)
+
+@app.on_event("startup")
+async def startup_event():
+    # generate a baseline caddy config on startup so the caddy server can run this bad boy
+    regenerate_caddy_config()
+
 @app.get("/")
 def _index():
     return "Running..."
@@ -55,35 +86,9 @@ def _run_app(project_name: str):
     # after the project has been started, add it to the in-memory store
     projects_store[project_name] = [project_port, ps]
 
-
     # regenerate caddy config such that it includes the newly added proejct
     regenerate_caddy_config()
     return { "success": True, "port": project_port } 
-
-# this function will regenerate the caddy config based on the newly added proejct paths and their ports
-def regenerate_caddy_config() -> None:
-    caddy_config = f"""
-        {ROOT_DOMAIN} {{
-            handle {{
-                respond "TerminalCraft Project Demo"
-            }}
-        }}
-    """
-
-    for project_name, project_info in projects_store.items():
-        project_caddy_config = f"""
-        {project_name}.{ROOT_DOMAIN} {{
-            reverse_proxy localhost:{project_info[0]}
-        }}
-        """
-
-        caddy_config += project_caddy_config
-
-    with open('Caddyfile', "w") as config_f:
-        config_f.write(caddy_config)
-
-    # logging the newly generated caddy config soley for debugging purposes
-    print(caddy_config)
 
 if __name__ == "__main__":
     uvicorn.run("expose:app", host="0.0.0.0", port=APP_PORT, reload=True)
